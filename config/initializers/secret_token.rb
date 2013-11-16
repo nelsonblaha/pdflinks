724237f9b9b01e059e3420950fa67a689e5574ff
Ben Nelson <nelson.blaha@gmail.com>
2013-11-16 10:40:09 -0600

    Initial commit, parses document bodies for apparent colleyville tx addresses and converts them to Google Maps links

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pdflinks::Application.config.secret_key_base = '5c68720f7cb96c72ffd7d2411251e97092528ff8538c9c6f188e01d2f871ea3be747fc5c4f76d08ed5f64faaf4d47c87a2702dcb99829e2e49effa9d9cefa787'
